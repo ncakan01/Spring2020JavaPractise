4f42eedc325b1df520bcfec834e4b0704e18e922
nurgul01 <ncakan01@gmail.com>
2020-04-28 14:36:55 -0500

    Day 27 Class

--- src/day27_DataTime/Dates_Formatting.java
+++ src/day27_DataTime/Dates_Formatting.java
@@ -1,4 +1,35 @@
 package day27_DataTime;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Dates_Formatting {
+    public static void main(String[] args) {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MMM/yyyy");   // mmm olursa april yazar  mm olursa 04 yazar
+
+
+        LocalDate date1 = LocalDate.now();
+        System.out.println(date1.format(dtf));
+
+        /*
+        CREATE A DATE called birthday = month/days/year
+        ex:     2020 04 23
+        APRIL/23/2020
+         */
+
+        DateTimeFormatter dtf2=DateTimeFormatter.ofPattern("EEE MMMM/dd/yyyy");   // mmm olursa april yazar  mm olursa 04 yazar  mmmm full name
+
+        LocalDate date2= LocalDate.of(1999,12,25);
+        String str1=date2.format(dtf2);
+        System.out.println(str1);
+        System.out.println("==================================================================");
+
+        LocalDate date3= LocalDate.now();
+
+        System.out.println(date3.format(dtf2));
+
+    }
 }
+
+
--- src/day27_DataTime/find_Minimum.java
+++ src/day27_DataTime/find_Minimum.java
@@ -1,4 +1,56 @@
 package day27_DataTime;
 
 public class find_Minimum {
+    public static void main(String[] args) {
+        int [] arr ={5,4,7,5};
+
+
+        int minimum=arr[0];
+        for (int each : arr){ // each : 5 , 4, 7 ,5
+
+            if(each < minimum){
+                minimum=each;
+            }
+
+        }
+        System.out.println(minimum);
+
+        System.out.println("========================================================================");
+
+        int [] arr2={1000,400,700,7,8,-4,-9};
+        System.out.println(minimum(arr2));
+
+        System.out.println("=========================================================================");
+
+        double [] arr3={55.0,10.5,40.0,1.5,2.5 ,0};
+        double num1=minimum(arr3);
+        System.out.println(num1);
+
+    }
+    public static int minimum(int [] arr){
+
+        int minimum=arr[0];
+        for (int each : arr){ // each : 5 , 4, 7 ,5
+
+            if(each < minimum){
+                minimum=each;
+            }
+
+        }
+
+        return minimum;
+    }
+    public static double minimum(double [] arr){
+
+
+        double minimum=arr[0];
+        for ( double each: arr){ // each : 5 , 4, 7 ,5
+
+            if(each < minimum){
+                minimum=each;
+            }
+
+        }
+        return minimum;
+    }
 }
